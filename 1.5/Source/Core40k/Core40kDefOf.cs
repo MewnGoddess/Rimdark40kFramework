a5aa817c2208878986f9af314bced9550910e245
Phonicmas <jkskovlund@gmail.com>
2024-06-14 12:22:40 +0200

    Re-added Warp flame damage + typo

--- 1.5/Source/Core40k/Core40kDefOf.cs
+++ 1.5/Source/Core40k/Core40kDefOf.cs
@@ -6,6 +6,8 @@ namespace Core40k
     [DefOf]
     public static class Core40kDefOf
     {
+        public static DamageDef BEWH_WarpFlame;
+
         static Core40kDefOf()
         {
             DefOfHelper.EnsureInitializedInCtor(typeof(Core40kDefOf));
