dfeb12335d1dda6dd0154c51a282dc4cab92176f
Phonicmas <jkskovlund@gmail.com>
2024-06-14 10:00:06 +0200

    Initial Framework Release

---
+++ 1.5/Source/Core40k/Comps/CompProperties_ForceWeapon.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace Core40k
+{
+    public class CompProperties_ForceWeapon : CompProperties
+    {
+        public DamageDef damageDef;
+
+        public List<ToolCapacityDef> capacitiesToApplyOn;
+
+        public StatDef scalingStat;
+        public float damageScalingFactor = 1;
+
+        public bool scalesPen = false;
+        public float penScaleFactor = 1;
+        public float flatPen = 1;
+
+        public CompProperties_ForceWeapon()
+        {
+            compClass = typeof(CompForceWeapon);
+        }
+    }
+}
---
+++ 1.5/Source/Core40k/Core40kDefOf.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Core40k
+{
+    [DefOf]
+    public static class Core40kDefOf
+    {
+        static Core40kDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(Core40kDefOf));
+        }
+    }
+}
