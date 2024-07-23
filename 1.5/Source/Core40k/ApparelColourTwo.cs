a0df85566adc0f35430574bf30725aba58aa6a75
Phonicmas <jkskovlund@gmail.com>
2024-07-23 22:29:39 +0200

    Two colour apparel

---
+++ 1.5/Source/Core40k/ApparelColourTwo.cs
@@ -0,0 +1,74 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+
+namespace Core40k
+{
+    [StaticConstructorOnStartup]
+    public class ApparelColourTwo : Apparel
+    {
+        private Color drawColorTwo = Color.red;
+
+        private Color drawColorOne = Color.white;
+
+        private static readonly Texture2D ChangePlumeColourIcon = ContentFinder<Texture2D>.Get("UI/Genes/AeldariPsyker_icon");
+
+        public override Color DrawColor
+        {
+            get
+            {
+                CompColorable comp = GetComp<CompColorable>();
+                if (comp != null && comp.Active)
+                {
+                    return comp.Color;
+                }
+                return drawColorOne;
+            }
+        }
+
+        public override Color DrawColorTwo
+        {
+            get
+            {
+                return drawColorTwo;
+            }
+        }
+        public override IEnumerable<Gizmo> GetWornGizmos()
+        {
+            foreach (Gizmo gizmo in base.GetGizmos())
+            {
+                yield return gizmo;
+            }
+
+            Command_Action command_Action1 = new Command_Action();
+            command_Action1.defaultLabel = "BEWH.ChangeSecondaryColour".Translate();
+            command_Action1.defaultDesc = "BEWH.ChangeSecondaryColourDesc".Translate(def.label);
+            command_Action1.icon = ChangePlumeColourIcon;
+            command_Action1.action = delegate
+            {
+                Find.WindowStack.Add(new Dialog_PaintSecondaryColour(this, this.Wearer));
+            };
+            yield return command_Action1;
+        }
+
+        public void SetSecondaryColor(Color color)
+        {
+            drawColorTwo = color;
+            Notify_ColorChanged();
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref drawColorTwo, "drawColorTwo", Color.red);
+            Scribe_Values.Look(ref drawColorOne, "drawColorOne", Color.white);
+
+            if (Scribe.mode == LoadSaveMode.PostLoadInit)
+            {
+                Notify_ColorChanged();
+            }
+        }
+    }
+}
---
+++ 1.5/Source/Core40k/Dialog_PaintSecondaryColour.cs
@@ -0,0 +1,169 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+
+namespace Core40k
+{
+    [StaticConstructorOnStartup]
+    public class Dialog_PaintSecondaryColour : Window
+    {
+        private readonly ApparelColourTwo apparel;
+
+        private Pawn pawn;
+
+        private Color chosenColour;
+
+        private static readonly Texture2D FavoriteColorTex = ContentFinder<Texture2D>.Get("UI/Icons/ColorSelector/ColorFavourite");
+
+        private static readonly Texture2D IdeoColorTex = ContentFinder<Texture2D>.Get("UI/Icons/ColorSelector/ColorIdeology");
+
+        private static readonly Vector2 ButSize = new Vector2(200f, 40f);
+
+        private bool DevMode => Prefs.DevMode;
+
+        private List<Color> allColors;
+
+        private List<Color> AllColors
+        {
+            get
+            {
+                if (allColors == null)
+                {
+                    allColors = new List<Color>();
+                    if (pawn.Ideo != null && !Find.IdeoManager.classicMode)
+                    {
+                        allColors.Add(pawn.Ideo.ApparelColor);
+                    }
+                    if (pawn.story != null && !pawn.DevelopmentalStage.Baby() && pawn.story.favoriteColor.HasValue && !allColors.Any((Color c) => pawn.story.favoriteColor.Value.IndistinguishableFrom(c)))
+                    {
+                        allColors.Add(pawn.story.favoriteColor.Value);
+                    }
+                    foreach (ColorDef colDef in DefDatabase<ColorDef>.AllDefs.Where((ColorDef x) => x.colorType == ColorType.Ideo || x.colorType == ColorType.Misc || (DevMode && !ModsConfig.IdeologyActive && x.colorType == ColorType.Structure)))
+                    {
+                        if (!allColors.Any((Color x) => x.IndistinguishableFrom(colDef.color)))
+                        {
+                            allColors.Add(colDef.color);
+                        }
+                    }
+                    allColors.SortByColor((Color x) => x);
+                }
+                return allColors;
+            }
+        }
+
+        public Dialog_PaintSecondaryColour()
+        { }
+
+        public Dialog_PaintSecondaryColour(ApparelColourTwo apparel, Pawn pawn)
+        {
+            this.apparel = apparel;
+            this.pawn = pawn;
+            chosenColour = apparel.DrawColorTwo;
+        }
+
+        public override void DoWindowContents(Rect inRect)
+        {
+            Text.Font = GameFont.Medium;
+            Rect rect = new Rect(inRect);
+            rect.height = Text.LineHeight * 2f;
+            Widgets.Label(rect, "BEWH.ChangeSecondaryColour".Translate().CapitalizeFirst());
+            Text.Font = GameFont.Small;
+            inRect.yMin = rect.yMax + 4f;
+            Rect rect2 = inRect;
+            DrawApparelColor(rect2);
+            DrawBottomButtons(inRect);
+        }
+
+        private void ColorSelecterExtraOnGUI(Color color, Rect boxRect)
+        {
+            Texture2D texture2D = null;
+            TaggedString taggedString = null;
+            bool flag = Mouse.IsOver(boxRect);
+            Pawn_StoryTracker story = pawn.story;
+            if (story != null && story.favoriteColor.HasValue && color.IndistinguishableFrom(pawn.story.favoriteColor.Value))
+            {
+                texture2D = FavoriteColorTex;
+                if (flag)
+                {
+                    taggedString = "FavoriteColorPickerTip".Translate(pawn.Named("PAWN"));
+                }
+            }
+            else if (pawn.Ideo != null && !Find.IdeoManager.classicMode && color.IndistinguishableFrom(pawn.Ideo.ApparelColor))
+            {
+                texture2D = IdeoColorTex;
+                if (flag)
+                {
+                    taggedString = "IdeoColorPickerTip".Translate(pawn.Named("PAWN"));
+                }
+            }
+            if (texture2D != null)
+            {
+                Rect position = boxRect.ContractedBy(4f);
+                GUI.color = Color.black.ToTransparent(0.2f);
+                GUI.DrawTexture(new Rect(position.x + 2f, position.y + 2f, position.width, position.height), texture2D);
+                GUI.color = Color.white.ToTransparent(0.8f);
+                GUI.DrawTexture(position, texture2D);
+                GUI.color = Color.white;
+            }
+            if (!taggedString.NullOrEmpty())
+            {
+                TooltipHandler.TipRegion(boxRect, taggedString);
+            }
+        }
+
+        private void DrawApparelColor(Rect rect)
+        {
+            float curY = rect.y;
+            Rect rect2 = new Rect(rect.x, curY, rect.width, 92f);
+            curY += rect2.height + 10f;
+            if (!pawn.apparel.IsLocked(apparel) || DevMode)
+            {
+                Widgets.ColorSelector(rect2, ref chosenColour, AllColors, out var _, Widgets.GetIconFor(apparel.def, null, apparel.StyleDef), 22, 2, ColorSelecterExtraOnGUI);
+                float num2 = rect2.x;
+                if (pawn.Ideo != null && !Find.IdeoManager.classicMode)
+                {
+                    rect2 = new Rect(num2, curY, 200f, 24f);
+                    if (Widgets.ButtonText(rect2, "SetIdeoColor".Translate()))
+                    {
+                        chosenColour = pawn.Ideo.ApparelColor;
+                    }
+                    num2 += 210f;
+                }
+                Pawn_StoryTracker story = pawn.story;
+                if (story != null && story.favoriteColor.HasValue)
+                {
+                    rect2 = new Rect(num2, curY, 200f, 24f);
+                    if (Widgets.ButtonText(rect2, "SetFavoriteColor".Translate()))
+                    {
+                        chosenColour = pawn.story.favoriteColor.Value;
+                    }
+                }
+            }
+            else
+            {
+                Widgets.ColorSelectorIcon(new Rect(rect2.x, rect2.y, 88f, 88f), apparel.def.uiIcon, chosenColour);
+                Text.Anchor = TextAnchor.MiddleLeft;
+                Rect rect3 = rect2;
+                rect3.x += 100f;
+                Widgets.Label(rect3, ((string)"ApparelLockedCannotRecolor".Translate(pawn.Named("PAWN"), apparel.Named("APPAREL"))).Colorize(ColorLibrary.RedReadable));
+                Text.Anchor = TextAnchor.UpperLeft;
+            }
+            curY += 34f;
+        }
+
+        private void DrawBottomButtons(Rect inRect)
+        {
+            if (Widgets.ButtonText(new Rect(inRect.x, inRect.yMax - ButSize.y, ButSize.x, ButSize.y), "Cancel".Translate()))
+            {
+                Close();
+            }
+            if (Widgets.ButtonText(new Rect(inRect.xMax - ButSize.x, inRect.yMax - ButSize.y, ButSize.x, ButSize.y), "Accept".Translate()))
+            {
+                apparel.SetSecondaryColor(chosenColour);
+            }
+        }
+    }
+}
