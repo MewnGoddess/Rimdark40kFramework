3a108f3513dd97947a9cd9e413ae0daef75431ba
Phonicmas <jkskovlund@gmail.com>
2024-12-13 10:46:13 +0100

    geneclass

---
+++ 1.5/Source/Core40k/DefModExtensions/DefModExtension_GeneDisabledBy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace Genes40k
+{
+    public class DefModExtension_GeneDisabledBy : DefModExtension
+    {
+        public List<GeneDef> geneDisabledBy = new List<GeneDef>();
+    }
+}
---
+++ 1.5/Source/Core40k/GeneClasses/Gene_DisabledBy.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Verse;
+
+namespace Genes40k
+{
+    public class Gene_DisabledBy : Gene
+    {
+        public override bool Active
+        {
+            get
+            {
+                if (def.HasModExtension<DefModExtension_GeneDisabledBy>())
+                {
+                    var disabledByGenes = def.GetModExtension<DefModExtension_GeneDisabledBy>().geneDisabledBy;
+                    if (Enumerable.Any(disabledByGenes, gene => pawn.genes.HasActiveGene(gene)))
+                    {
+                        return false;
+                    }
+                }
+                return base.Active;
+            }
+        }
+    }
+}
