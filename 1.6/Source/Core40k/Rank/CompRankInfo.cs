7047d531a809b3e6976c5a63c905d39353090be1
Phonicmas <jkskovlund@gmail.com>
2025-09-14 16:49:23 +0200

    new fix

--- 1.6/Source/Core40k/Rank/CompRankInfo.cs
+++ 1.6/Source/Core40k/Rank/CompRankInfo.cs
@@ -17,6 +17,7 @@ public class CompRankInfo : ThingComp
     {
         get
         {
+            unlockedRanks ??= [];
             if (unlockedRanks.Contains(null))
             {
                 unlockedRanks.RemoveAll(def => def == null);
